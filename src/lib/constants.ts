e707f3f28ec4f1ec7dcb5c6e118072453ad558f3
lobsanag1765 <lobsang1765@gmail.com>
2025-01-21 12:20:59 +0545

    new feature add from beta vesrion on morning

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,8 +4,8 @@ export const APP_VERSION = version;
 export const IS_BETA = true;
 
 export const RPC_URL =
-  // 'https://mainnet.helius-rpc.com/?api-key=744e34ff-5991-4207-8cb4-d64ea2581e12';
-  // process.env.NEXT_PUBLIC_HELIUS_RPC_URL ||
-  'https://api.mainnet-beta.solana.com';
+  'https://mainnet.helius-rpc.com/?api-key=744e34ff-5991-4207-8cb4-d64ea2581e12';
+// process.env.NEXT_PUBLIC_HELIUS_RPC_URL ||
+// 'https://api.mainnet-beta.solana.com';
 
 export const MAX_TOKEN_MESSAGES = 10;
