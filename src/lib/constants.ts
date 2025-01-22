aa765d55bc1c339339ee5b3156d9bb3b06f96e2b
kreatorland <kreatorland@gmail.com>
2025-01-22 20:54:52 +0545

    update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,4 +5,4 @@ export const IS_BETA = true;
 
 export const RPC_URL = 'https://mainnet.helius-rpc.com/?api-key=744e34ff-5991-4207-8cb4-d64ea2581e12';
 
-export const MAX_TOKEN_MESSAGES = 10;
+export const MAX_TOKEN_MESSAGES = 30;
